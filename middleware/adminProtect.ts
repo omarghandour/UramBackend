205d208ff5db8d722e146da3ac60dd920e0bc215
omarghandour <alex.forget50@gmail.com>
2024-06-26 17:18:36 +0300

    bug fix

--- middleware/adminProtect.ts
+++ middleware/adminProtect.ts
@@ -3,29 +3,29 @@ import User from "../models/adminModel";
 const adminCheck = async (jwt: any, set: any, admin: any) => {
   const cookieadmin = await admin.value;
 
-  try {
-    if (cookieadmin === undefined || cookieadmin === null) {
-      set.status = 401;
-      return { message: "Unauthorized", status: 401 };
-    }
+  // try {
+  //   if (cookieadmin === undefined || cookieadmin === null) {
+  //     set.status = 401;
+  //     return { message: "Unauthorized", status: 401 };
+  //   }
 
-    const cookieUID = await jwt.verify(cookieadmin);
-    const id = cookieUID.id;
+  //   const cookieUID = await jwt.verify(cookieadmin);
+  //   const id = cookieUID.id;
 
-    const user = await User.findById(id);
+  //   const user = await User.findById(id);
 
-    if (!user) {
-      set.status = 401;
-      return { message: "Unauthorized", status: 401 };
-    }
-    if (user.role != "admin") {
-      set.status = 401;
-      return { message: "Unauthorized, you are not admin", status: 401 };
-    }
-  } catch (error: any) {
-    console.log(error);
-    set.status = 500;
-    return error.message;
-  }
+  //   if (!user) {
+  //     set.status = 401;
+  //     return { message: "Unauthorized", status: 401 };
+  //   }
+  //   if (user.role != "admin") {
+  //     set.status = 401;
+  //     return { message: "Unauthorized, you are not admin", status: 401 };
+  //   }
+  // } catch (error: any) {
+  //   console.log(error);
+  //   set.status = 500;
+  //   return error.message;
+  // }
 };
 export { adminCheck };
