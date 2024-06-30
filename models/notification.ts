6e2070dee8ba4be2fab79f2865535d901fbb4842
omarghandour <alex.forget50@gmail.com>
2024-06-30 16:19:54 +0300

    test

--- models/notification.ts
+++ models/notification.ts
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const notification = new mongoose.Schema({
-  name: String,
-  description: String,
-  photo: String,
-});
+const notification = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    photo: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 const Notification = mongoose.model("Notification", notification);
 export default Notification;
