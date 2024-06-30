94978d4f446a2d8e7b7dde9ed7b8f72ff56dd105
omarghandour <alex.forget50@gmail.com>
2024-06-30 03:18:31 +0300

    Rating schema update

--- models/ratingModel.ts
+++ models/ratingModel.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const RatingSchema = new mongoose.Schema({
   score: Number,
+  type: String,
   judge: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   team: { type: mongoose.Schema.Types.ObjectId, ref: "Team" },
 });
